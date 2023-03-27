aded6cdb313a90b94962d9bb75f19b9fabc64e17
nilesjoel <nilesjoel@gmail.com>
2023-03-27 13:48:25 -0500

    Update Config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     styledComponents: true,
   },
   env : {
-    NEXTAUTH_URL : "http://studio-social-next.vercel.app:3000",
+    NEXTAUTH_URL : "http://studio-social-next.vercel.app",
     NEXTAUTH_API: "https://dashboard.studiosymmetries.com",
     NEXTAUTH_SECRET : "5ce5dcc2b8e8a2bd33bfcb398f2f290906706d5dc481f83dc5ad5e09998ac73d",
     GOOGLE_CLIENT_ID : "851508202208-eu684t55dskvrjiatuh67ranbdnvd9va.apps.googleusercontent.com",
